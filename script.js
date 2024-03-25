7d97dc702027e6ec611c44f1b141cc8bbd36ecd6
accio-rajitha <159473339+accio-rajitha@users.noreply.github.com>
2024-03-25 15:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const byteSize = (str) => {
   // write your code here
+	// Encoding the string to UTF-8
+  const utf8Bytes = new TextEncoder().encode(str);
+  // Returning the size of the UTF-8 encoded bytes
+  return utf8Bytes.length;
 };
 
 // Do not change the code below
